daab0c2249848e443045e395d0316bf5a64f1875
Sara Romao <sara-romao@live.com>
2025-10-20 19:25:45 -0300

    modified send msg and added cron

--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,35 @@
 import 'dotenv/config';
 import Fastify from 'fastify';
-import {reportPriceCripto } from './cripto_get';
+import cron from 'node-cron';
+import { reportPriceCripto } from './cripto_get';
 const fastify = Fastify({
   logger: true
 })
 
-interface MessageRequest {
-  chatId: number | string;
-}
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.BOT_TOKEN;
+const chatId = process.env.CHATID;
+const bot = new TelegramBot(token, { polling: true });
 
-// async function getMe() {
-//   try {
-//     const response = await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getUpdates `);
-//     const data = await response.json();
+bot.onText(/\/start/, () => {
 
-//     console.log(data.result[1].message);
+  async function report() {
+    const result_coins = await reportPriceCripto();
 
-//   } catch (error) {
-//     console.log({ error: 'Failed to fetch data' });
-//   }
-
-// }
-// getMe();
-
-
-// interface MessageRequest {
-//   chatId: number | string;
-//   message: string;
-//   cripto: string;
-// }
-
-fastify.post('/send-message', async function (req, reply) {
-  console.log("body", req.body);
-  const result_coins = await reportPriceCripto();
-
-  try {
-    const { chatId} = req.body as MessageRequest;
-    const text = result_coins;
-
-    const response = await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: text,
-
-      })
-    });
-
-    const data = await response.json();
-    console.log('Mensagem enviada:', data);
-    reply.send(data);
-
-  } catch (error) {
-    reply.status(500).send({ error: 'Failed to fetch data' });
+    bot.sendMessage(chatId, result_coins);
   }
+
+  report();
+
+});
+
+cron.schedule('20 17 * * *', () => {
+  async function report() {
+    const result_coins = await reportPriceCripto();
+    bot.sendMessage(chatId, result_coins);
+  }
+
+  report();
 });
 
 fastify.listen({ port: 3000 }, function (err) {
